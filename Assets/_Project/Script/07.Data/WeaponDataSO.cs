9b7b751f28138ffd925e5ab90b204716edaf8b85
OzCoding2-ParkYeounjoo <parj8877@naver.com>
2026-01-25 21:23:38 +0900

    SO 관련 정보 정리

--- Assets/_Project/Script/07.Data/WeaponDataSO.cs
+++ Assets/_Project/Script/07.Data/WeaponDataSO.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum WeaponType { Ranged, Melee}
 [CreateAssetMenu(fileName = "NewWeapon",menuName = "SO/Weapon")]
 public class WeaponDataSO : ScriptableObject
 {
     [Header("Info")]
     public string weaponName;
+    public WeaponType weaponType;
     [TextArea] public string descrption;
     public Sprite icon;
 
